be7e7eb4679179edd13fa37496fd22a9bafd1271
57qy654 <Jude.Pitschka@gmail.com>
2024-11-06 15:02:05 -0600

    Components and Styles Folders

---
+++ src/Pages-Components/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import '../Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
