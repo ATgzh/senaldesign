95cd72c5c4675cdf32dce11b6984ac73e604d23e
57qy654 <Jude.Pitschka@gmail.com>
2024-11-06 16:21:22 -0600

    Initial Navbar

--- src/Pages-Components/App.tsx
+++ src/Pages-Components/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route, Link, useLocation} from 'react-router-dom';
 import '../Styles/App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello
-        </p>
-      </header>
-    </div>
+    <main>
+      <Router>
+        <Navbar/>
+      <Routes>
+        
+      </Routes>
+    </Router>
+    </main>
   );
 }
 
